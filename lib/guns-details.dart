6dc2aecbc45d3b9650695a8bf422c37fb0491928
Danilo Almeida Alves <71449479+b0d3-crypt@users.noreply.github.com>
2024-06-16 10:01:50 -0300

    Update guns-details.dart

--- lib/guns-details.dart
+++ lib/guns-details.dart
@@ -11,7 +11,7 @@ class GunsDetails extends StatefulWidget {
 }
 
 class _GunsDetailsState extends State<GunsDetails> {
-  bool isLiked = false;
+  bool liked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,50 +19,77 @@ class _GunsDetailsState extends State<GunsDetails> {
       appBar: AppBar(
         title: Text(widget.gun.name),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                widget.gun.image,
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 200,
-              ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Text(
-                    widget.gun.name,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isLiked = !isLiked;
-                    });
-                  },
-                  icon: Icon(
-                    isLiked ? Icons.favorite : Icons.favorite_border,
-                    color: Colors.red,
-                  ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
                 ),
               ],
             ),
-            SizedBox(height: 8),
-            Text(
-              widget.gun.description,
-              style: TextStyle(fontSize: 18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Imagem da arma
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  child: Image.asset(
+                    widget.gun.image,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 200,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          widget.gun.name,
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            liked = !liked;
+                          });
+                        },
+                        child: Icon(
+                          liked ? Icons.favorite : Icons.favorite_border,
+                          color: liked ? Colors.red : null,
+                          size: 32,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    widget.gun.description,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                SizedBox(height: 16),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
